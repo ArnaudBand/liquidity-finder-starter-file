31e8021a9284b0ed4bb11ccf1a865297c7803936
ArnaudBand <bandonkeyea@gmail.com>
2024-01-10 09:49:50 +0300

    Refactor import statements and add state variables

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from "react";
 
 import {
   Header,
@@ -16,12 +16,25 @@ import {
 import { Context } from "../context/index";
 
 const index = () => {
-  const { Dapp_Name } = useContext(Context);
+  const { Dapp_Name, loading, GET_POOL_ADDRESS, GET_POOL_DETAILS } =
+    useContext(Context);
+
+  // STATE VARIABLE
+  const [activeNetwork, setActiveNetwork] = useState("");
+  const [activeComponent, setActiveComponent] = useState("Home");
+
+  useEffect(() => {
+    const network = JSON.parse(localStorage.getItem("activeNetwork"));
+    setActiveNetwork(network?.name);
+  }, [activeNetwork]);
   return (
-    <div>
-      {Dapp_Name}
+    <div className="bg-slate-900">
+      <Header
+        setActiveComponent={setActiveComponent}
+        activeNetwork={activeNetwork}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default index;
